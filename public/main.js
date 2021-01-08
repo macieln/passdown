a3fe2fa044026556e30cd34fb7c785ff8d990531
Nahum Maciel <nahumamaciel@gmail.com>
2021-01-08 11:59:55 -0800

    Set up basic work orders view & nav menu.

---
+++ public/main.js
@@ -0,0 +1,9 @@
+function openWo(woId) {
+    console.log("Opened WO: " + woId)
+};
+
+var newWObuttonEl = document.getElementById('newWoButton');
+
+newWObuttonEl.addEventListener("click", function(){
+    console.log("What up Coco!");
+});
